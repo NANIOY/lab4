bfe12ab189f98e7c27643a78ed3cba44fdf31616
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-01 15:47:21 +0100

    feat: add user

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -2,33 +2,54 @@
 const Message = require("../../../models/Message");
 
 const index = async (req, res) => {
-    let messages = await Message.find({});
-    res.json({
-        status: "success",
-        message: "GET all messages",
-        data: [
-            {
-                messages: messages,
-            },
-        ],
-    });
+    try {
+        let messages = await Message.find({}).populate("user");
+
+        res.json({
+            status: "success",
+            message: "GET all messages",
+            data: [
+                {
+                    messages: messages.map((message) => ({
+                        _id: message._id,
+                        message: message.message,
+                        user: message.user,
+                        __v: message.__v,
+                    })),
+                },
+            ],
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 const create = async (req, res) => {
     let message = req.body.message;
-    let m = new Message();
-    m.message = message;
-    await m.save();
+    let user = req.body.user;
 
-    res.json({
-        status: "success",
-        message: "POST a new message",
-        data: [
-            {
-                message: m,
-            },
-        ],
-    });
+    try {
+        const m = new Message({
+            message: message,
+            user: user,
+        });
+
+        const savedMessage = await m.save();
+
+        res.json({
+            status: "success",
+            message: "POST a new message",
+            data: [
+                {
+                    message: savedMessage,
+                },
+            ],
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 module.exports.index = index;
