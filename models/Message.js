f71e4b5e2eed51452f5dde2fb002ace4f76064bf
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-01 16:09:12 +0100

    feat: add user reference

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,7 @@
-// create a mongoose schema for messages
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+
 const MessageSchema = new Schema({
-    // message is a required string
     message: {
         type: String,
         required: true,
@@ -10,8 +9,9 @@ const MessageSchema = new Schema({
     user: {
         type: Schema.Types.ObjectId,
         ref: "User",
+        required: true,
     },
 });
-// export the model to use it in index.js
+
 const Message = mongoose.model("Message", MessageSchema);
 module.exports = Message;
