974c4894f9c125cd412c50d780cbef5e054bac03
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-01 16:18:59 +0100

    feat: add username filter

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -2,19 +2,40 @@ const Message = require("../../../models/Message");
 const User = require("../../../models/User");
 
 const index = async (req, res) => {
-    let messages = await Message.find({}).populate({
-        path: "user",
-        select: "username",
-    });
-    res.json({
-        status: "success",
-        message: "GET all messages",
-        data: [
-            {
-                messages: messages,
-            },
-        ],
-    });
+    try {
+        const { username } = req.query;
+        let messages;
+
+        if (username) {
+            messages = await Message.find({})
+                .populate({
+                    path: "user",
+                    match: { username },
+                });
+        } else {
+            messages = await Message.find({}).populate("user");
+        }
+
+        const filteredMessages = messages.filter((message) => message.user !== null);
+
+        res.json({
+            status: "success",
+            message: "GET messages for user",
+            data: [
+                {
+                    messages: filteredMessages.map((message) => ({
+                        _id: message._id,
+                        message: message.message,
+                        user: message.user.username,
+                        __v: message.__v,
+                    })),
+                },
+            ],
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
 };
 
 const create = async (req, res) => {
