8dd5ca57d5575cc301f405da70975926ee430336
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-01 15:28:28 +0100

    chore: remove pug

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const path = require('path');
 const app = express();
 const port = 3000;
 
@@ -10,8 +9,8 @@ const cors = require("cors");
 app.use(cors());
 
 mongoose.connect(process.env.MONGODB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 // console log .env MONGODB
@@ -21,30 +20,12 @@ console.log(process.env.MONGODB);
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 
-// configure Pug
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views")); // create "views" directory
-
 // import routes
 const messagesRouter = require("./routes/api/v1/messages");
 app.use(express.json());
 
 // use routes
 app.use("/api/v1/messages", messagesRouter);
-
-// define route to render Pug template
-app.get('/', async (req, res) => {
-    try {
-        const response = await fetch('https://nodejs-messages.onrender.com/api/v1/messages');
-        const data = await response.json();
-        console.log(data);
-        const messages = data[0].messages;
-        res.render('index', { messages });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send('Internal Server Error');
-    }
-});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
