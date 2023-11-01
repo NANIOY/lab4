85cd74c1db0ef5fb05f583c667c510f94c95eaf9
Yannick Van den Brande <yavadebr@gmail.com>
2023-11-01 17:15:19 +0100

    feat: add comments

--- controllers/api/v1/messages.js
+++ controllers/api/v1/messages.js
@@ -3,11 +3,16 @@ const User = require("../../../models/User");
 
 const index = async (req, res) => {
     try {
+        // get the username from the request query
         const { username } = req.query;
+        
+        // fetch all messages and populate the 'user' field with the 'username' attribute
         const messages = await Message.find({}).populate("user", "username");
 
+        // filter messages based on the provided username or return all messages
         const filteredMessages = username ? messages.filter((message) => message.user.username === username) : messages;
 
+        // send a JSON response with the filtered messages
         res.json({
             status: "success",
             message: "GET messages for user",
@@ -19,28 +24,42 @@ const index = async (req, res) => {
     }
 };
 
+// create new messages
 const create = async (req, res) => {
-    const { message: messageText, user: username } = req.body;
+    try {
+        // extract message text and username from the request body
+        const { message: messageText, user: username } = req.body;
 
-    let user = await User.findOne({ username }) || new User({ username });
+        // find an existing user or create a new one based on the provided username
+        let user = await User.findOne({ username }) || new User({ username });
 
-    const newMessage = await Message.create({ message: messageText, user });
+        // create a new message using the message text and user
+        const newMessage = await Message.create({ message: messageText, user });
 
-    res.json({
-        status: "success",
-        message: "POST a new message",
-        data: [{ message: newMessage }],
-    });
+        // send a JSON response with the newly created message
+        res.json({
+            status: "success",
+            message: "POST a new message",
+            data: [{ message: newMessage }],
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ status: "error", message: "Internal Server Error" });
+    }
 };
 
+// filter by message id
 const getMessageById = async (req, res) => {
     try {
+        // get a message by its unique ID and populate the 'user' field with 'username'
         const message = await Message.findById(req.params.id).populate("user", "username");
 
+        // if the message is not found, return a 404 error response
         if (!message) {
             return res.status(404).json({ status: "error", message: "Message not found" });
         }
 
+        // send a JSON response with the requested message
         res.json({
             status: "success",
             message: "GET a message by ID",
@@ -52,18 +71,23 @@ const getMessageById = async (req, res) => {
     }
 };
 
+// update message by id
 const updateMessage = async (req, res) => {
     try {
+        // get the message ID from the request parameters and the updated message text from the request body
         const { id } = req.params;
         const { message: updatedMessage } = req.body;
 
+        // find and update the message by its ID, populating the 'user' field with 'username'
         const message = await Message.findByIdAndUpdate(id, { message: updatedMessage }, { new: true })
             .populate("user", "username");
 
+        // if the message is not found, return a 404 error response
         if (!message) {
             return res.status(404).json({ status: "error", message: "Message not found" });
         }
 
+        // send a JSON response with the updated message and username
         res.json({
             status: "success",
             message: "Message updated successfully",
@@ -75,16 +99,21 @@ const updateMessage = async (req, res) => {
     }
 };
 
+// delete message by id
 const deleteMessage = async (req, res) => {
     try {
+        // get the message ID from the request parameters
         const { id } = req.params;
 
+        // find and delete the message by its ID
         const message = await Message.findByIdAndDelete(id);
 
+        // if the message is not found, return a 404 error response
         if (!message) {
             return res.status(404).json({ status: "error", message: "Message not found" });
         }
 
+        // send a success response indicating that the message was removed
         res.json({
             status: "success",
             message: "The message was removed",
@@ -95,6 +124,7 @@ const deleteMessage = async (req, res) => {
     }
 };
 
+// export controller functions as methods
 module.exports = {
     index,
     create,
--- routes/api/v1/messages.js
+++ routes/api/v1/messages.js
@@ -6,11 +6,15 @@ const router = express.Router();
 // import controller
 const messagesController = require("../../../controllers/api/v1/messages");
 
+// GET all messages
 router.get("/", messagesController.index);
+// POST new message
 router.post("/", messagesController.create);
-
+// GET message by id
 router.get("/:id", messagesController.getMessageById);
+// PUT update message by id
 router.put("/:id", messagesController.updateMessage);
+// DELETE message by id
 router.delete("/:id", messagesController.deleteMessage);
 
 module.exports = router;
